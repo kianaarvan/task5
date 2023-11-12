d35bda6bcc95f281bfbc19aa43be79180c07c983
kianaarvan <148455828+kianaarvan@users.noreply.github.com>
2023-11-12 16:28:16 +0330

    Add files via upload

---
+++ task5b.py
@@ -0,0 +1,10 @@
+n=int(input("enter n:"))
+m=int(input("enter m:"))
+for i in range (n):
+    for j in range (m):
+      if (i+j)%2==0:
+        print("#",end="")
+      else:
+        print("*",end="")
+    print()
+    
